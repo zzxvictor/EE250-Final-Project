3e93b0384387f414e2ee2fdd4faca046aba8001a
Zixuan Victor Zhang <35340469+zzxvictor@users.noreply.github.com>
2018-04-20 23:21:46 -0700

    Update ultrasonicBeacon.py

--- ultrasonicBeacon.py
+++ ultrasonicBeacon.py
@@ -10,10 +10,11 @@ def readSerial():
   sensorList2 = []
   x=ser.readline()
   data = x.decode("utf-8")
-  print (data)
-  data.split(',')
-  sensorList1.append(data[0])
-  sensorList2.append(data[1])
+  dataList = data.split(',')
+  print (dataList )
+  
+  sensorList1.append(dataList[0])
+  sensorList2.append(dataList[1])
   sensorList1 = sensorList1[-1*LEGNTH:]
   sensorList2 = sensorList2[-1*LEGNTH:]
   
