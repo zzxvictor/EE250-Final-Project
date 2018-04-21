f7cad1441536ea4b23fa854e4fdf863cdd10c3e8
Zixuan Victor Zhang <35340469+zzxvictor@users.noreply.github.com>
2018-04-21 15:24:47 -0700

    Update ultrasonicBeacon.py

--- ultrasonicBeacon.py
+++ ultrasonicBeacon.py
@@ -1,9 +1,11 @@
 import time
 import serial
 import re
+import pandas
+
 ser = serial.Serial(port='/dev/ttyACM0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 WINDOW = 5
-LEGNTH = 50
+LEGNTH = 30
 sensorList1 = []
 sensorList2 = []
 
@@ -21,14 +23,22 @@ def readSerial():
     
   sensorList1 = sensorList1[-1*LEGNTH:]
   sensorList2 = sensorList2[-1*LEGNTH:]
- 
 
+  
+def signalProcessing():
+  global sensorList1
+  global sensorList2
+  #moving average filter
+  sensorList1 = pandas.rolling_mean(sensorList1, WINDOW)
+  sensorList2 = pandas.rolling_mean(sensorList2, WINDOW)
+  
+  
 
 def main ():
   while(True):
     readSerial()
-    print (sensorList1)
-    print (sensorList2)
+    signalProcessing()
+    
   
 main ()
   
