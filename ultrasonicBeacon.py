4ffb9c3e411f33d3ac2b1f725c834567102c07d9
Zixuan Victor Zhang <35340469+zzxvictor@users.noreply.github.com>
2018-04-21 15:14:31 -0700

    Update ultrasonicBeacon.py

--- ultrasonicBeacon.py
+++ ultrasonicBeacon.py
@@ -4,11 +4,12 @@ import re
 ser = serial.Serial(port='/dev/ttyACM0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 WINDOW = 5
 LEGNTH = 50
-
+sensorList1 = []
+sensorList2 = []
 
 def readSerial():
-  sensorList1 = []
-  sensorList2 = []
+  global sensorList1 
+  global sensorList2 
   x=ser.readline()
   data = x.decode()
   results = re.split('[+ \r \n]',data)
@@ -20,13 +21,12 @@ def readSerial():
     
   sensorList1 = sensorList1[-1*LEGNTH:]
   sensorList2 = sensorList2[-1*LEGNTH:]
-  
-  return sensorList1, sensorList2
+ 
 
 
 def main ():
   while(True):
-    sensorList1, sensorList2 = readSerial()
+    readSerial()
     print (sensorList1)
     print (sensorList2)
   
